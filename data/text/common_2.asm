a6cb67f8780be607b76797655dd3a2a6dd0b3f65
RaJah239 <ravijagroo239@gmail.com>
2025-04-26 16:28:40 -0400

    Prevent depositing TM/HMs and Key Items into the PC Credit: https://github.com/pret/pokecrystal/wiki/Infinitely-reusable-TMs#7-prevent-depositing-tmhms-into-the-pc

--- data/text/common_2.asm
+++ data/text/common_2.asm
@@ -1115,6 +1115,11 @@ _PlayersPCNoItemsText::
 	text "No items here!"
 	prompt
 
+_CantDepositText::
+	text "TM/HM or Key Items"
+	line "can't be deposited."
+	prompt
+
 _PlayersPCHowManyDepositText::
 	text "How many do you"
 	line "want to deposit?"
